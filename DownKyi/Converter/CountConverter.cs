2ffa86420a75bc3e593c81b81cac4f510f74d027
croire <1432593898@qq.com>
2021-12-23 13:17:56 +0800

    下载列表添加无数据显示

---
+++ DownKyi/Converter/CountConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DownKyi.Converter
+{
+    public class CountConverter : IValueConverter
+    {
+        public int Count { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)value) > Count;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
