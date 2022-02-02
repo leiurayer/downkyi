4c09ded9c1da7dfaf0118040f7000ebc7c428164
croire <1432593898@qq.com>
2022-02-02 10:06:45 +0800

    调整UserAgent

--- DownKyi.Core/BiliApi/WebClient.cs
+++ DownKyi.Core/BiliApi/WebClient.cs
@@ -51,7 +51,7 @@ namespace DownKyi.Core.BiliApi
                 request.Timeout = 30 * 1000;
 
                 // MacOS Safari
-                string safari = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15";
+                //string safari = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15";
                 // Windows 10 Chrome
                 string chrome = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36";
                 request.UserAgent = chrome;
