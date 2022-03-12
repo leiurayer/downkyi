440ebff101b11c37f491e18951e2c052bb1786a7
croire <1432593898@qq.com>
2022-03-12 23:29:26 +0800

    修改版本号到1.5.0-alpha4

--- DownKyi/Models/AppInfo.cs
+++ DownKyi/Models/AppInfo.cs
@@ -3,12 +3,12 @@
     public class AppInfo
     {
         public string Name { get; } = "哔哩下载姬";
-        public int VersionCode { get; } = 502;
+        public int VersionCode { get; } = 503;
 
 #if DEBUG
-        public string VersionName { get; } = "1.5.0-alpha3 Debug";
+        public string VersionName { get; } = "1.5.0-alpha4 Debug";
 #else
-        public string VersionName { get; } = "1.5.0-alpha3";
+        public string VersionName { get; } = "1.5.0-alpha4";
 #endif
 
     }
