084d4ab363a25a7c29b05e267ca9ec0525ed4a56
croire <1432593898@qq.com>
2022-03-15 19:43:44 +0800

    整理代码格式

--- DownKyi/Services/Download/AriaDownloadService.cs
+++ DownKyi/Services/Download/AriaDownloadService.cs
@@ -142,20 +142,26 @@ namespace DownKyi.Services.Download
 
             // 下载文件名
             string fileName = Guid.NewGuid().ToString("N");
-
             string key = $"{downloadVideo.Id}_{downloadVideo.Codecs}";
+
             // 老版本数据库没有这一项，会变成null
             if (downloading.Downloading.DownloadedFiles == null)
+            {
                 downloading.Downloading.DownloadedFiles = new List<string>();
+            }
+
             if (downloading.Downloading.DownloadFiles.ContainsKey(key))
             {
                 // 如果存在，表示下载过，
                 // 则继续使用上次下载的文件名
                 fileName = downloading.Downloading.DownloadFiles[key];
+
                 // 还要检查一下文件有没有被人删掉，删掉的话重新下载
                 // 如果下载视频之后音频文件被人删了。此时gid还是视频的，会下错文件
                 if (downloading.Downloading.DownloadedFiles.Contains(key) && File.Exists(Path.Combine(path, fileName)))
+                {
                     return Path.Combine(path, fileName);
+                }
             }
             else
             {
