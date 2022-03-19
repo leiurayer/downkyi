4f10bbf1b699900d25bc13d439596b4c732d9b0a
leiurayer <1432593898@qq.com>
2022-03-20 00:30:05 +0800

    发布v1.5.0-alpha6

--- src/DownKyi/Models/AppInfo.cs
+++ src/DownKyi/Models/AppInfo.cs
@@ -3,12 +3,12 @@
     public class AppInfo
     {
         public string Name { get; } = "哔哩下载姬";
-        public int VersionCode { get; } = 504;
+        public int VersionCode { get; } = 505;
 
 #if DEBUG
-        public string VersionName { get; } = "1.5.0-alpha5 Debug";
+        public string VersionName { get; } = "1.5.0-alpha6 Debug";
 #else
-        public string VersionName { get; } = "1.5.0-alpha5";
+        public string VersionName { get; } = "1.5.0-alpha6";
 #endif
 
     }
