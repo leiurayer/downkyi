755790aae09a3e13fae91803f10e1a739ab44b72
croire <1432593898@qq.com>
2022-06-05 12:00:57 +0800

    修改版本号到1.5.3

--- DownKyi/Models/AppInfo.cs
+++ DownKyi/Models/AppInfo.cs
@@ -3,12 +3,12 @@
     public class AppInfo
     {
         public string Name { get; } = "哔哩下载姬";
-        public int VersionCode { get; } = 509;
+        public int VersionCode { get; } = 510;
 
 #if DEBUG
-        public string VersionName { get; } = "1.5.2 Debug";
+        public string VersionName { get; } = "1.5.3 Debug";
 #else
-        public string VersionName { get; } = "1.5.2";
+        public string VersionName { get; } = "1.5.3";
 #endif
 
     }
