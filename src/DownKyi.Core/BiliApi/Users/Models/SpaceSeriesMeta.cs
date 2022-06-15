3b7005572993eecf439e714b648f528fbb0a0116
leiurayer <1432593898@qq.com>
2022-06-16 00:18:00 +0800

    支持UP主用户空间的合集和列表

---
+++ src/DownKyi.Core/BiliApi/Users/Models/SpaceSeriesMeta.cs
@@ -0,0 +1,28 @@
+﻿using DownKyi.Core.BiliApi.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DownKyi.Core.BiliApi.Users.Models
+{
+    // https://api.bilibili.com/x/series/series?series_id={seriesId}
+    public class SpaceSeriesMetaOrigin : BaseModel
+    {
+        //[JsonProperty("code")]
+        //public int Code { get; set; }
+        //[JsonProperty("message")]
+        //public string Message { get; set; }
+        //[JsonProperty("ttl")]
+        //public int Ttl { get; set; }
+        [JsonProperty("data")]
+        public SpaceSeriesMetaData Data { get; set; }
+    }
+
+    public class SpaceSeriesMetaData : BaseModel
+    {
+        [JsonProperty("meta")]
+        public SpaceSeriesMeta Meta { get; set; }
+        [JsonProperty("recent_aids")]
+        public List<long> RecentAids { get; set; }
+    }
+
+}
