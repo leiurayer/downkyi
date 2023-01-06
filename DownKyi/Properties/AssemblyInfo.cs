dc4033cd8b504a7cfabf0cb92d6cb48292c79525
croire <1432593898@qq.com>
2023-01-06 16:59:36 +0800

    修改版本号到1.5.6

--- DownKyi/Properties/AssemblyInfo.cs
+++ DownKyi/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DownKyi")]
 [assembly: AssemblyProduct("DownKyi")]
-[assembly: AssemblyCopyright("Copyright ©  Downkyi 2020-2022")]
+[assembly: AssemblyCopyright("Copyright ©  Downkyi 2020-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.5.0")]
-[assembly: AssemblyFileVersion("1.5.5.0")]
+[assembly: AssemblyVersion("1.5.6.0")]
+[assembly: AssemblyFileVersion("1.5.6.0")]
